78bb76ed64d0b5a6881d3c24bc8c2150e26adb73
zveriu <zveriu@7066a82a-f19d-11de-8355-bf90974c4141>
2010-04-11 18:49:43 +0000

    - proxmark3 log external parser (simple python)  - proxmark3 full log recovery option added (-P)  - fingerprint base implementation and few templates

--- trunk/src/build_cygwin.sh
+++ trunk/src/build_cygwin.sh
@@ -3,4 +3,4 @@ LIBNFC=libnfc
 CFLAGS=`pkg-config --cflags ${LIBNFC}`
 CFLAGS_LIBNFC=`pkg-config --cflags libnfc | cut -d ' ' -f 1`/${LIBNFC}
 
-gcc ./${MAIN_FILE}.c ./mfcuk_mifare.c ./mfcuk_utils.c ./crapto1.c ./crypto1.c ./bin/libnfc.lib ${CFLAGS} ${CFLAGS_LIBNFC} -o ./bin/${MAIN_FILE}_cygwin.exe
+gcc ./${MAIN_FILE}.c ./mfcuk_mifare.c ./mfcuk_utils.c ./mfcuk_finger.c ./crapto1.c ./crypto1.c ./bin/libnfc.lib ${CFLAGS} ${CFLAGS_LIBNFC} -o ./bin/${MAIN_FILE}_cygwin.exe
